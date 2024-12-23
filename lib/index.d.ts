e0ca41d50276f402cfbe16c9a04d8737ae66a1b9
ndtrong <nguyenductrong12a1@gmail.com>
2024-12-24 01:08:22 +0700

    Update: Re-build package

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,5 @@
 import { backendProto } from './grpc/definition';
 import KafkaListener, { TopicResolver } from './kafka/listener';
 import KafkaProducer, { PublishPayload } from './kafka/producer';
-export { KafkaListener, KafkaProducer };
-export { backendProto, TopicResolver, PublishPayload, };
+export { backendProto, KafkaListener, KafkaProducer, TopicResolver, PublishPayload, };
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import { backendProto } from './grpc/definition';
 import KafkaListener, { TopicResolver } from './kafka/listener';
 import KafkaProducer, { PublishPayload } from './kafka/producer';
 
-// Default export
-export { KafkaListener, KafkaProducer };
-
-// Named exports
 export {
     backendProto,
+    KafkaListener,
+    KafkaProducer,
     TopicResolver,
     PublishPayload,
 };
