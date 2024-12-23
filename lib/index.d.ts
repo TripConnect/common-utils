a203d5fe4be6045a56812dacf3b5ad8f08e5ffca
ndtrong <nguyenductrong12a1@gmail.com>
2024-12-23 22:50:47 +0700

    Update: Re-build the package

---
+++ lib/index.d.ts
@@ -0,0 +1,5 @@
+import { backendProto } from './grpc/definition';
+import KafkaListener from './kafka/listener';
+import KafkaProducer from './kafka/producer';
+export { backendProto, KafkaListener, KafkaProducer };
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { backendProto } from './grpc/definition';
+import KafkaListener from './kafka/listener';
+import KafkaProducer from './kafka/producer';
+
+export {
+    backendProto,
+    KafkaListener,
+    KafkaProducer
+};
