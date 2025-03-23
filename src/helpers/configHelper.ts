e4be2a4e8b472aeb00b83aea7d9a6282b36f45db
ndtrong <nguyenductrong12a1@gmail.com>
2025-03-23 14:44:13 +0700

    Update: Define ConfigHelper.readWithDefault

--- src/helpers/configHelper.ts
+++ src/helpers/configHelper.ts
@@ -7,7 +7,7 @@ export default class ConfigHelper {
     private static configs: Record<string, any>;
 
     /**
-     * Reading the service configurations
+     * Reading the service configuration by key
      * @param path The path to sprcific configuration key
      * @returns The configuration value
      * @example
@@ -25,6 +25,32 @@ export default class ConfigHelper {
             return JSON.parse(resp.getBody("utf-8"));
         }
 
-        return path.split('.').reduce((obj, key) => obj?.[key], ConfigHelper.configs);
+        let configValue = path.split('.').reduce((obj, key) => obj?.[key], ConfigHelper.configs);
+        if (!configValue) throw new Error("Config not found: " + path);
+        return configValue;
+    }
+
+    /**
+     * Reading the service configuration by key, return default value if not found
+     * @param path The path to sprcific configuration key
+     * @param defaultValue The default value will be returned if the configuration not found
+     * @returns The configuration value
+     * @example
+     * ConfigHelper.readWithDefault("server.port", 3107);
+     */
+    public static readWithDefault(path: string, defaultValue: any): any {
+        if (!ConfigHelper.configs) {
+            let configHost = process.env.NODE_ENV === "development" ? "localhost" : "config-service";
+            let serviceName = process.env.SERVICE_NAME;
+            let configUrl = `http://${configHost}:31070/configs/${serviceName}`;
+            let resp = request("GET", configUrl);
+            if (resp.isError()) {
+                throw new Error("The configuration cannot loaded");
+            }
+            return JSON.parse(resp.getBody("utf-8"));
+        }
+
+        let configValue = path.split('.').reduce((obj, key) => obj?.[key], ConfigHelper.configs);
+        return configValue ?? defaultValue;
     }
 }
