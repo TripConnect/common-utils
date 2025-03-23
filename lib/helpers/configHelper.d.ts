28dac9e84e893d02d87fa629820e7d591ae39f09
ndtrong <nguyenductrong12a1@gmail.com>
2025-03-23 14:44:32 +0700

    Update: Build package

--- lib/helpers/configHelper.d.ts
+++ lib/helpers/configHelper.d.ts
@@ -2,12 +2,21 @@ import "dotenv/config";
 export default class ConfigHelper {
     private static configs;
     /**
-     * Reading the service configurations
+     * Reading the service configuration by key
      * @param path The path to sprcific configuration key
      * @returns The configuration value
      * @example
      * ConfigHelper.read("server.port");
      */
     static read(path: string): any;
+    /**
+     * Reading the service configuration by key, return default value if not found
+     * @param path The path to sprcific configuration key
+     * @param defaultValue The default value will be returned if the configuration not found
+     * @returns The configuration value
+     * @example
+     * ConfigHelper.readWithDefault("server.port", 3107);
+     */
+    static readWithDefault(path: string, defaultValue: any): any;
 }
 //# sourceMappingURL=configHelper.d.ts.map
