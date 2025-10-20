29f1dc80fa5a750a3f75f38651107f6a4a30a6f3
ndtrong <nguyenductrong12a1@gmail.com>
2025-10-20 23:48:34 +0700

    style: update docstring

--- src/helpers/tokenHelper.ts
+++ src/helpers/tokenHelper.ts
@@ -2,37 +2,37 @@ import * as jwt from 'jsonwebtoken';
 import ConfigHelper from './configHelper';
 
 export type JwtBody = {
-    userId: string
+  userId: string
 }
 
 export default class TokenHelper {
 
-    private static get SECRET(): string {
-        return ConfigHelper.read("jwt-secret-key");
+  private static get SECRET(): string {
+    return ConfigHelper.read("jwt-secret-key");
+  }
+
+  /**
+   * Verify jwt token to get
+   * @param token The jwt token
+   * @returns The jwt body from given valid token, otherwise null returned
+   */
+  public static verify(token: string): JwtBody | null {
+    try {
+      return jwt.verify(token, TokenHelper.SECRET) as JwtBody;
+    } catch (ignored) {
+      return null;
     }
 
-    /**
-     * Verify jwt token to get
-     * @param token The jwt token
-     * @returns The jwt body from given valid token, otherwise null returned
-     */
-    public static verify(token: string): JwtBody | null {
-        try {
-            return jwt.verify(token, TokenHelper.SECRET) as JwtBody;
-        } catch (ignored) {
-            return null;
-        }
+  }
 
-    }
-
-    /**
-     * Sign jwt token
-     * @param token The jwt token
-     * @returns The jwt token
-     */
-    public static sign(user: JwtBody): string {
-        let accessToken = jwt.sign(user, TokenHelper.SECRET);
-        return accessToken;
-    }
+  /**
+   * Sign jwt token
+   * @param user The jwt token body
+   * @returns The jwt token
+   */
+  public static sign(user: JwtBody): string {
+    let accessToken = jwt.sign(user, TokenHelper.SECRET);
+    return accessToken;
+  }
 
 }
